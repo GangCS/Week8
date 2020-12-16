ef0be96a44e0e1169e923c7d9db0d29157c1666b
YosefTwito <57401163+YosefTwito@users.noreply.github.com>
2020-12-16 16:08:31 +0200

    Update AllowedTilesQ6.cs

--- Assets/Scenes/Week8-Homework/Q6/AllowedTilesQ6.cs
+++ Assets/Scenes/Week8-Homework/Q6/AllowedTilesQ6.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class AllowedTilesQ6 : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     [SerializeField] TileBase[] allowedTiles = null;
     public bool Contain(TileBase tile)
     {
         return allowedTiles.Contains(tile);
-
     }
 
-    public TileBase[] Get() { return allowedTiles; }
+    public TileBase[] Get() 
+    {
+        return allowedTiles; 
+    }
 }
